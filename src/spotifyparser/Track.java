c845f7c0662356a58346b505540363c28c55990c
cstuser <cstuser>
2018-10-29 15:37:38 -0400

    Assignment Push #1

---
+++ src/spotifyparser/Track.java
@@ -0,0 +1,50 @@
+package spotifyparser;
+
+public class Track {
+    private int number;
+    private String title;
+    private int durationInSeconds;
+    private String url;
+
+    public Track(int number, String title, int durationInSeconds, String url)
+    {
+        this.number = number;
+        this.title = title;
+        this.durationInSeconds = durationInSeconds;
+        this.url = url;
+    }
+    
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getDurationInSeconds() {
+        return durationInSeconds;
+    }
+
+    public void setDurationInSeconds(int durationInSeconds) {
+        this.durationInSeconds = durationInSeconds;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrlL(String url) {
+        this.url = url;
+    }
+
+    
+}
---
+++ src/spotifyparser/TrackForTableView.java
@@ -0,0 +1,54 @@
+package spotifyparser;
+
+
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class TrackForTableView{
+    private SimpleIntegerProperty trackNumber;
+    private SimpleStringProperty trackTitle;
+    private SimpleIntegerProperty trackDurationInSeconds;
+    private SimpleStringProperty trackPreviewUrl;
+    
+    public TrackForTableView()
+    {
+        trackNumber = new SimpleIntegerProperty();
+        trackTitle = new SimpleStringProperty();
+        trackDurationInSeconds = new SimpleIntegerProperty();
+        trackPreviewUrl = new SimpleStringProperty();
+    }
+    
+    public int getTrackNumber() {
+        return trackNumber.getValue();
+    }
+
+    public void setTrackNumber(int trackNumber) {
+        this.trackNumber.set(trackNumber);
+    }
+
+    public String getTrackTitle() {
+        return trackTitle.getValue();
+    }
+
+    public void setTrackTitle(String trackTitle) {
+        this.trackTitle.set(trackTitle);
+    }
+    
+    public int getTrackDurationInSeconds() {
+        return trackDurationInSeconds.getValue();
+    }
+
+    public void setTrackDurationInSeconds(int trackNumber) {
+        this.trackDurationInSeconds.set(trackNumber);
+    }
+
+    public String getTrackPreviewUrl()
+    {
+        return trackPreviewUrl.get();
+    }
+    
+    public void setTrackPreviewUrl(String trackPreviewUrl)
+    {
+        this.trackPreviewUrl.set(trackPreviewUrl);
+    }    
+}
